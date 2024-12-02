f625ab713bba69643cddc0d7cdd60f67e8420bea
Theo <putzelfutzel@gmx.net>
2024-12-02 12:39:51 +0100

    Update: Introduce responsive NavBar

--- src/app/[lang]/components/navbar/_navBar.tsx
+++ src/app/[lang]/components/navbar/_navBar.tsx
@@ -1,32 +1,42 @@
 "use client";
 
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { useTranslations } from "next-intl";
 
 import LocaleSwitcher from "@/app/[lang]/components/navbar/_localeSwitcher";
 import { Link } from "@/i18n/routing";
 
+import styles from "./navbar.module.scss";
+
 export default function NavBar() {
     const t = useTranslations("Navigation");
     return (
-        <Nav defaultActiveKey='/' className='flex-row'>
-            <Nav.Link as={Link} href={`/about`}>
-                {t("about")}
-            </Nav.Link>
-            <Nav.Link as={Link} href={`/skills`}>
-                {t("skills")}
-            </Nav.Link>
-            <Nav.Link as={Link} href={`/projects`}>
-                {t("projects")}
-            </Nav.Link>
-            <Nav.Link as={Link} href={`/other`}>
-                {t("other")}
-            </Nav.Link>
-            <Nav.Link as={Link} href={`/fun-facts`}>
-                {t("funFacts")}
-            </Nav.Link>
-            <LocaleSwitcher />
-        </Nav>
+        <Navbar className={styles.navBar} expand='lg' variant={"dark"}>
+            <Navbar.Brand as={Link} href={`/`}>
+                Millyscript
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav' className={" justify-content-end"}>
+                <Nav className='d-flex align-items-lg-center align-items-end gap-2 gap-lg-0'>
+                    <Nav.Link as={Link} href={`/about`} className={styles.navLink}>
+                        {t("about")}
+                    </Nav.Link>
+                    <Nav.Link as={Link} href={`/skills`} className={styles.navLink}>
+                        {t("skills")}
+                    </Nav.Link>
+                    <Nav.Link as={Link} href={`/projects`} className={styles.navLink}>
+                        {t("projects")}
+                    </Nav.Link>
+                    <Nav.Link as={Link} href={`/other`} className={styles.navLink}>
+                        {t("other")}
+                    </Nav.Link>
+                    <Nav.Link as={Link} href={`/fun-facts`} className={styles.navLink}>
+                        {t("funFacts")}
+                    </Nav.Link>
+                    <LocaleSwitcher />
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
