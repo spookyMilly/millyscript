dec5409612a2a01778fae149d747e559cf7b40e8
Theo <putzelfutzel@gmx.net>
2024-12-02 11:09:20 +0100

    Update: Fix navbar links to work with locale, add page placeholders

--- src/app/[lang]/components/navbar/_navBar.tsx
+++ src/app/[lang]/components/navbar/_navBar.tsx
@@ -3,16 +3,27 @@ import { Nav } from "react-bootstrap";
 import { useTranslations } from "next-intl";
 
 import LocaleSwitcher from "@/app/[lang]/components/navbar/_localeSwitcher";
+import { Link } from "@/i18n/routing";
 
 export default function NavBar() {
     const t = useTranslations("Navigation");
     return (
         <Nav defaultActiveKey='/' className='flex-column'>
-            <Nav.Link href={"/about"}>{t("about")}</Nav.Link>
-            <Nav.Link href={"/skills"}>{t("skills")}</Nav.Link>
-            <Nav.Link href={"/projects"}>{t("projects")}</Nav.Link>
-            <Nav.Link href={"/other"}>{t("other")}</Nav.Link>
-            <Nav.Link href={"/fun-facts"}>{t("funFacts")}</Nav.Link>
+            <Nav.Link as={Link} href={`/about`}>
+                {t("about")}
+            </Nav.Link>
+            <Nav.Link as={Link} href={`/skills`}>
+                {t("skills")}
+            </Nav.Link>
+            <Nav.Link as={Link} href={`/projects`}>
+                {t("projects")}
+            </Nav.Link>
+            <Nav.Link as={Link} href={`/other`}>
+                {t("other")}
+            </Nav.Link>
+            <Nav.Link as={Link} href={`/fun-facts`}>
+                {t("funFacts")}
+            </Nav.Link>
             <LocaleSwitcher />
         </Nav>
     );
---
+++ src/app/[lang]/fun-facts/page.tsx
@@ -0,0 +1,3 @@
+export default function FunFacts() {
+    return <div>Fun facts</div>;
+}
