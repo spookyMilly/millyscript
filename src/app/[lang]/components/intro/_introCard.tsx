b9ec3a61217dc45c28c14cad3719288173d8e7e2
Theo <putzelfutzel@gmx.net>
2025-01-18 09:19:59 +0100

    Update: Improve translations and overall responsiveness, especially for small devices

--- src/app/[lang]/components/intro/_introCard.tsx
+++ src/app/[lang]/components/intro/_introCard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 import avatar from "@/app/static/intro.jpg";
+import avatar_small from "@/app/static/square.jpg";
 import { Link } from "@/i18n/routing";
 
 import styles from "./introCard.module.scss";
@@ -10,18 +11,26 @@ import styles from "./introCard.module.scss";
 export default function IntroCard() {
     const t = useTranslations("Intro");
     return (
-        <div className={"d-flex flex-lg-row flex-column gap-4"}>
-            <div>
+        <div className={"d-flex flex-lg-row flex-column gap-lg-4 gap-0 align-items-center"}>
+            <div className='d-none d-lg-block'>
                 <Image
-                    className={styles.avatar}
+                    className={styles.avatarBig}
                     src={avatar}
                     alt='Ein Foto von mir hoch oben in den Bergen in Norwegen'
                     priority
                 />
             </div>
+            <div className={"d-block d-lg-none"}>
+                <Image
+                    src={avatar_small}
+                    className={styles.avatarSmall}
+                    alt={"Ein Foto von mir hoch oben in den Bergen in Norwegen"}
+                    priority
+                />
+            </div>
             <div>
                 <h1>{t("heading")}</h1>
-                <p className={"fs-4"} dangerouslySetInnerHTML={{ __html: t("description") }} />
+                <p className={"fs-5"} dangerouslySetInnerHTML={{ __html: t("description") }} />
                 <p>
                     {t.rich("linkToProjects", {
                         link: (chunks) => <Link href='/projects'>{chunks}</Link>,
